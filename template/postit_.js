31cba7af462d31b1cb860ea9217ee3e24b9ae4c0
Rufix_Duke <rufixduke09@gmail.com>
2021-01-12 12:13:36 +0100

    App Template

--- template/postit_.js
+++ template/postit_.js
@@ -37,12 +37,53 @@ function loginPage(s) {
 
 function messagePage(s) {
     // event.preventDefault();
-    let myGroup = document.getElementById('id').value;
-    let myMessage = document.getElementById('msg2').value;
+   // let myGroup = document.getElementById('id').value;
+   // let myMessage = document.getElementById('msg2').value;
     // document.getElementsByClassName('mail')
     // console.log(myEmailValue, ' lllssss ', document.getElementsByClassName('mail'));
-    alert(JSON.stringify({
-        group: myGroup,
-        message: myMessage
-    }));
+    var myGroup = document.createElement("span")
+    // var myMessage = document.createElement("span");
+    myGroup.innerText = document.getElementById('id').value + " " + document.getElementById('msg2').value;
+    // myMessage.innerText = document.getElementById('msg2').value;
+    // document.body.appendChild(myMessage);
+    document.body.appendChild(myGroup);
+};
+
+function contacts(s) {
+    console.log('hello, onnlooad')
+    var text = " ";
+    let arr = ["Qion", "Mikky", "Damilare"];
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        var myOl = document.getElementById("arr")
+        var myLi = document.createElement("li");
+        myLi.onclick = function handleclick() {
+            // console.log('nooooooo')
+            myArray = {
+                1: "I'm a boy",
+                2: "Hello",
+                3: "What's up??"
+             }
+             var myLis = document.createElement("p");
+             myLis.innerText = 'hello';
+             document.body.appendChild(myLis);
+         };
+        console.log(myLi)
+        myLi.innerText = arr[i]
+        myOl.appendChild(myLi)
+        // text += arr[i];
+    }
+    // document.getElemen/tById("arr").innerHTML = text;
 }
+
+// function handleclick() {
+//    // console.log('nooooooo')
+//    myArray = {
+//        1: "I'm a boy",
+//        2: "Hello",
+//        3: "What's up??"
+//     }
+//     var myLis = document.createElement("p");
+//     myLis.innerText = document.getElementById("arr").value;
+//     document.body.appendChild(myLis);
+// }
